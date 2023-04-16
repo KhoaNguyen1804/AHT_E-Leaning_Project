8c1d4e9dd58a595b9776144d5e9d45debffb6e0e
BI11-122 <101243446+KhoaNguyen1804@users.noreply.github.com>
2023-04-16 23:51:53 +0700

    react

---
+++ react/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVital from "./components/ReportWebVital";
+import GlobalStyles from '~/components/GlobalStyles'
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
+    </React.StrictMode>
+);
+
+
+reportWebVital();
